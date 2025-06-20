405a5c0ec56967f39684a25fb8366b9fd555b2c8
ignaciolp <ignaciolp@lepa.com.ar>
2025-06-20 16:47:29 -0300

    termino ejercicio 2

--- JS/ejercicio2.js
+++ JS/ejercicio2.js
@@ -7,12 +7,12 @@ do {
 
 const longCiudades = ciudades.length;
 
-document.writeln(`<p class=p-3>Arreglo generado: </p>`);
+/* document.writeln(`<p class=p-3>Arreglo generado: </p>`);
 document.writeln(`<ul>`);
 for (i = 0; i < longCiudades; i++) {
   document.writeln(`<li>${ciudades[i]}</li>`);
 }
-document.writeln(`</ul>`);
+document.writeln(`</ul>`); */
 
 document.writeln(`El arreglo de ciudades tiene ${longCiudades} elementos`);
 
@@ -24,3 +24,14 @@ document.writeln(`<ul class=mt-2>
   <li> Elemento ultima posicion: ${ciudades[longCiudades - 1]}</li>
 </ul>`);
 
+ciudades.push(`Paris`);
+ciudades.splice(1, 0, `España`);
+const longUltima = ciudades.length;
+document.writeln(`Elemento ultima posicion: ${ciudades[longUltima - 1]}`);
+
+document.writeln(`<h4 class=p-2> Arreglo de ciudades </h4>`);
+document.writeln(`<ul>`);
+for (i = 0; i < longUltima; i++) {
+  document.writeln(`<li>${ciudades[i]}</li>`);
+}
+document.writeln(`</ul>`);
